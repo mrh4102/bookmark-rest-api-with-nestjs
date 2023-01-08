593b032ec3f28e55d2e2bca71d4d4d9a7aa16b50
Michael Angeles <mrh4102c@gmail.com>
2023-01-08 15:22:27 +0800

    Added logic to bookmark service

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,85 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Bookmark } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateBookmarkDto, UpdateBookmarkDto } from './dto';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  constructor(private prismaService: PrismaService) {}
+
+  async selectAllBookmarks(userId: number): Promise<Bookmark[]> {
+    const bookmarks = await this.prismaService.bookmark.findMany({
+      where: { userId },
+    });
+    return bookmarks;
+  }
+
+  async createBookmarkItem(
+    userId: number,
+    dto: CreateBookmarkDto,
+  ): Promise<Bookmark> {
+    const bookmark = await this.prismaService.bookmark.create({
+      data: { userId, ...dto },
+    });
+    return bookmark;
+  }
+
+  async selectBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ): Promise<Bookmark> {
+    const bookmark = await this.prismaService.bookmark.findUnique({
+      where: { id: bookmarkId },
+    });
+    if (!bookmark) {
+      throw new NotFoundException();
+    }
+    if (bookmark.userId !== userId) {
+      throw new ForbiddenException();
+    }
+    return bookmark;
+  }
+
+  async updateBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: UpdateBookmarkDto,
+  ): Promise<Bookmark> {
+    const bookmark = await this.prismaService.bookmark.findUnique({
+      where: { id: bookmarkId },
+    });
+    if (!bookmark) {
+      throw new NotFoundException();
+    }
+    if (bookmark.userId !== userId) {
+      throw new ForbiddenException();
+    }
+    return await this.prismaService.bookmark.update({
+      where: { id: bookmarkId },
+      data: { ...dto },
+    });
+  }
+
+  async deleteBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ): Promise<Bookmark> {
+    const bookmark = await this.prismaService.bookmark.findUnique({
+      where: { id: bookmarkId },
+    });
+    if (!bookmark) {
+      throw new NotFoundException();
+    }
+    if (bookmark.userId !== userId) {
+      throw new ForbiddenException();
+    }
+    return await this.prismaService.bookmark.delete({
+      where: { id: bookmarkId },
+    });
+  }
+}
