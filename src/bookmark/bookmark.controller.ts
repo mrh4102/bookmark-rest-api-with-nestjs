fc5b5e716ef212d1125714436f09960e61fc1758
Michael Angeles <mrh4102c@gmail.com>
2023-01-09 14:19:11 +0800

    Added logic to bookmark controller

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Bookmark } from '@prisma/client';
+
+import { User } from '../app.decorator';
+import { BookmarkService } from './bookmark.service';
+import { CreateBookmarkDto, UpdateBookmarkDto } from './dto';
 
 @Controller('bookmarks')
-export class BookmarkController {}
+@UsePipes(new ValidationPipe())
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+
+  @Get()
+  selectAllBookmarks(@User('id') userId: number): Promise<Bookmark[]> {
+    return this.bookmarkService.selectAllBookmarks(userId);
+  }
+
+  @Post()
+  createBookmarkItem(
+    @User('id') userId: number,
+    @Body() dto: CreateBookmarkDto,
+  ): Promise<Bookmark> {
+    return this.bookmarkService.createBookmarkItem(userId, dto);
+  }
+
+  @Get(':id')
+  selectBookmarkById(
+    @User('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ): Promise<Bookmark> {
+    return this.bookmarkService.selectBookmarkById(userId, bookmarkId);
+  }
+
+  @Patch(':id')
+  updateBookmarkById(
+    @User('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: UpdateBookmarkDto,
+  ): Promise<Bookmark> {
+    return this.bookmarkService.updateBookmarkById(userId, bookmarkId, dto);
+  }
+
+  @Delete(':id')
+  deleteBookmarkById(
+    @User('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ): Promise<Bookmark> {
+    return this.bookmarkService.deleteBookmarkById(userId, bookmarkId);
+  }
+}
