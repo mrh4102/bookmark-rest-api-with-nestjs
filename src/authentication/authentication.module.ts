19aca9bcb717ddb3af3b7d1869cd1925665d5778
Michael Angeles <mrh4102c@gmail.com>
2023-01-10 13:02:08 +0800

    Added prisma module as import to user module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaModule } from '../prisma/prisma.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
