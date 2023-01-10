383da7b0bea7b7e1ecefad9a5d81d1c98cc95b09
Michael Angeles <mrh4102c@gmail.com>
2023-01-10 16:48:45 +0800

    Added logic to authentication controller

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+
+import { AuthenticationService } from './authentication.service';
+import { UserDto } from './dto';
 
 @Controller()
-export class AuthenticationController {}
+@UsePipes(new ValidationPipe())
+export class AuthenticationController {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  @Post('signup')
+  signup(@Body() dto: UserDto): Promise<User> {
+    return this.authenticationService.signup(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: UserDto): Promise<User> {
+    return this.authenticationService.signin(dto);
+  }
+}
