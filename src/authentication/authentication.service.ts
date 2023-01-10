42c88a8002bd68d286c1db8cebd01f02737a4c62
Michael Angeles <mrh4102c@gmail.com>
2023-01-10 14:38:24 +0800

    Added logic to authentication service

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { UserDto } from './dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private prismaService: PrismaService) {}
+
+  async signup(dto: UserDto): Promise<User> {
+    try {
+      const user = await this.prismaService.user.create({
+        data: { ...dto },
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async signin(dto: UserDto): Promise<User> {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { username: dto.username },
+      });
+      if (!user) {
+        throw new NotFoundException();
+      }
+      if (user.password !== dto.password) {
+        throw new ForbiddenException();
+      }
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
