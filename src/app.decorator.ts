6dbb87f4de0dac08179280d1605d02a9c756034b
Michael Angeles <mrh4102c@gmail.com>
2023-01-11 16:50:53 +0800

    Enabled user decorator logic

--- src/app.decorator.ts
+++ src/app.decorator.ts
@@ -2,10 +2,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const User = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
-    return 1;
-
-    // TODO authentication and authorization
-
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
 
