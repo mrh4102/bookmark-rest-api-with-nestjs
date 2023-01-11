b83b8d7346bcd8b0a10619308dced193b4d62793
Michael Angeles <mrh4102c@gmail.com>
2023-01-11 10:40:23 +0800

    Added jwt module as import to authentication module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { jwtConstants } from './jwt/constants';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
